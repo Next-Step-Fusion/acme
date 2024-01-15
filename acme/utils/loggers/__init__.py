00c5817f340cf7b5c4c8c4231ad9f7806bb6535d
alex-gran23 <ag@nextfusion.org>
2024-01-15 07:11:19 +0300

    re-add MLFlowLogger to init

--- acme/utils/loggers/__init__.py
+++ acme/utils/loggers/__init__.py
@@ -34,6 +34,7 @@ from acme.utils.loggers.filters import NoneFilter
 from acme.utils.loggers.filters import TimeFilter
 from acme.utils.loggers.flatten import FlattenDictLogger
 from acme.utils.loggers.default import make_default_logger  # pylint: disable=g-bad-import-order
+from acme.utils.loggers.mlflow import MLFlowLogger
 from acme.utils.loggers.terminal import TerminalLogger
 from acme.utils.loggers.timestamp import TimestampLogger
 
